1b97cd8bf0ab8dca85e0f8bd7731801efb3fb2a3
fumiyasac <just1factory@gmail.com>
2025-02-07 11:42:27 +0900

    PostalCodeRepositoryを新規作成する

---
+++ lib/repositories/postal_code_repository.dart
@@ -0,0 +1,26 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/address.dart';
+
+part 'postal_code_repository.g.dart';
+
+@riverpod
+PostalCodeRepository postalCodeRepository(PostalCodeRepositoryRef ref) {
+  return PostalCodeRepository();
+}
+
+class PostalCodeRepository {
+  Future<Address> getAddress(String postalCode) async {
+    final cleanPostalCode = postalCode.replaceAll('-', '/');
+    final response = await http.get(
+      Uri.parse('https://madefor.github.io/postal-code-api/api/v1/$cleanPostalCode.json'),
+    );
+
+    if (response.statusCode == 200) {
+      return Address.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('住所の取得に失敗しました');
+    }
+  }
+}
---
+++ lib/repositories/postal_code_repository.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'postal_code_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$postalCodeRepositoryHash() =>
+    r'904c238b03798f1f20bd7e1113663d74415b6171';
+
+/// See also [postalCodeRepository].
+@ProviderFor(postalCodeRepository)
+final postalCodeRepositoryProvider =
+    AutoDisposeProvider<PostalCodeRepository>.internal(
+  postalCodeRepository,
+  name: r'postalCodeRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$postalCodeRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PostalCodeRepositoryRef = AutoDisposeProviderRef<PostalCodeRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
